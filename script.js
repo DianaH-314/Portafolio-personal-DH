1edc138fba6cbc52ec9c482cd6e5887042866bc0
DianaH-314 <dianalau230998@gmail.com>
2026-07-25 00:19:08 -0600

    Fix: estilo 1, cards y tipografía

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
 //=============================================
+//      Para la animación del navbar
+//=============================================
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.navbar a');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');      // Cambia el icono a X
+    navbar.classList.toggle('active');     // Oculta el menú cristal
+});
+
+// Evento de scroll
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header');
+	const menuIcon = document.querySelector('#menu-icon');
+    
+    // Aplica la clase según la ubicación
+    if (window.scrollY > 55) {
+        header.classList.add('cambio-fondo');
+		menuIcon.style.color = 'var(--color-Azure)';
+    } else {
+        header.classList.remove('cambio-fondo');
+		menuIcon.style.color = '#ffffff';
+    }
+});
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        const currentActive = document.querySelector('.navbar a.active');
+     
+        if (currentActive) {
+            currentActive.classList.remove('active');
+        }
+        
+        // Ponemos el color rosa en el que presionamos
+        link.classList.add('active');
+    });
+});
+
+
+
+//=============================================
 //      Para el carrousel de tecnologías
 //=============================================
 
